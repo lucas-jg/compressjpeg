b627ae0a32c24eb7c5e9da79d58426fc17796205
lucas <kjk727@me.com>
2020-02-19 11:30:26 +0900

    폴더에 있는 모든 파일 압축하도록 변경

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 const { execFile } = require("child_process");
 const mozjpeg = require("mozjpeg");
-
-if (!process.argv[2]) {
-	console.log("Quality(5 ~ 95)를 입력하세요. 종료합니다.");
-	return;
-}
+let path = require("path");
+let fs = require("fs");
 
 const OUT_FILE = "-outfile";
 const QUALITY = "-quality";
+const files = fs.readdirSync(path.resolve(__dirname, "../img"));
 
-const quality = process.argv[2];
-const inputAddress = "C:\\Users\\jkkim\\Documents\\compressjpeg\\img\\2.jpg";
-const outputAddress = `C:\\Users\\jkkim\\Documents\\compressjpeg\\img\\compress\\input${quality}.jpg`;
+let quality = 60;
 
-execFile(mozjpeg, [OUT_FILE, outputAddress, QUALITY, quality, inputAddress], err => {
-	console.log("Image minified! : " + err);
+if (!process.argv[2]) {
+  console.log(
+    "Quality(5 ~ 95)를 입력하세요. 값이 없는 경우 기본으로 60으로 설정됩니다."
+  );
+} else {
+  quality = process.argv[2];
+}
+
+files.forEach(file => {
+  if (file.search(".jpg") > -1) {
+    console.log(file);
+
+    let inputAddress = path.resolve(__dirname, "../img") + "\\" + file;
+    let outputAddress =
+      path.resolve(__dirname, "../img/compress") + "\\" + file;
+
+    execFile(
+      mozjpeg,
+      [OUT_FILE, outputAddress, QUALITY, quality, inputAddress],
+      err => {
+        console.log("Image minified. Output  : " + outputAddress);
+        console.log("Error : " + err);
+      }
+    );
+  }
 });
